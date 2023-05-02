e5fc64c8000756153d3be611caf4cf3e9cee864e
houssam72 <brothersmarket72@gmail.com>
2023-05-02 13:58:31 +0200

    Add Lesson 4

--- server.js
+++ server.js
@@ -1,7 +1,55 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-  console.log("Request made");
+  console.log(req.url, req.method);
+
+  res.setHeader("Content-Type", "text/html");
+  // res.setHeader('Content-Type','text/text')
+  //  res.write('Hellow Ninja')
+  //  res.end()
+
+  // send an html file
+  // fs.readFile("./views/index.html",(err,data)=>{
+  //   if(err){
+  //  console.log(err)
+  //  res.end()
+  //   } else{
+  //     res.write(data)
+  //     res.end()
+  //   }
+  // })
+
+  // Basic routing
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode=200
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode=200
+      break;
+    case '/about-me':
+      res.statusCode=301
+      res.setHeader('location','/about')
+      res.end()
+    default:
+      path += "404.html";
+      res.statusCode=404
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
